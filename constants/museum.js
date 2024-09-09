776b3c9d0d65edacd729c16e37d73717c074857b
Kierthana <kierthanars@gmail.com>
2024-09-09 13:57:21 +0530

    [kierthana] Added the script to chat bot

---
+++ constants/museum.js
@@ -0,0 +1,40 @@
+export default museumsData = [
+  {
+    id: 1,
+    name: "Museum A",
+    location: "City A",
+    description: "A great place to learn about art and history...",
+    category: "Art",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTllvBIeB9gxw1IissvAozsUm2HoH22mkuzHg&s",
+    duration: "2-3 hours",
+    ticketPrice: {
+      adult: "₹200",
+      child: "₹100",
+    },
+    additionalAttractions: ["Folk Dance", "3D Shows", "Puppet Show"],
+    openingTime: "09:00",
+    closingTime: "18:00",
+  },
+  {
+    id: 2,
+    name: "Museum B",
+    location: "City B",
+    description: "Explore the history of science and innovation...",
+    category: "Science",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpzX0hWhPrjhRRiJ72w8PelsiuvtCp0-RBJg&s",
+    duration: "3-4 hours",
+    ticketPrice: {
+      adult: "₹250",
+      child: "₹150",
+    },
+    additionalAttractions: [
+      "Virtual Reality Experience",
+      "Science Workshops",
+      "Planetarium Shows",
+    ],
+    openingTime: "10:00",
+    closingTime: "17:00",
+  },
+];
