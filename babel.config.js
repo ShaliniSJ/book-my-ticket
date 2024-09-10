c71e1ba52cd0000a4019a3428cdc1d226215f380
ShaliniSJ <s.j.shalini2005@gmail.com>
2024-09-10 22:40:11 +0530

    [Shalini]adds .env in .gitignore

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    plugins: ["nativewind/babel",{
+      "moduleName": "@env",
+      "path": ".env",
+      "blocklist": null,
+      "allowlist": null,
+      "safe": false,
+      "allowUndefined": true,
+      "verbose": false
+    }],
   };
 };
