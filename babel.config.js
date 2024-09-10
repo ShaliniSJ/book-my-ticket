b8a210c231c9a756a1e027771f855cec4291a3af
ShaliniSJ <s.j.shalini2005@gmail.com>
2024-09-10 22:49:50 +0530

    [Shalini]adds dotenv plugin

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel",{
-      "moduleName": "@env",
-      "path": ".env",
-      "blocklist": null,
-      "allowlist": null,
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false
-    }],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module:react-native-dotenv', // Correct plugin name for dotenv
+        {
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false
+        }
+      ]
+    ],
   };
 };
